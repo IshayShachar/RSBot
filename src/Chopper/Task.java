7b471c3a5ed4352d39919375a555f41a9f40982a
ishay <ishay3000@gmail.com>
2019-03-13 12:46:28 +0200

    Added more documentation to the Task class

--- src/Chopper/Task.java
+++ src/Chopper/Task.java
@@ -5,13 +5,21 @@ import org.powerbot.script.rt4.ClientContext;
 
 /**
  * A class represents a task to be done on the {@link org.powerbot.script.rt4.Player}
- * @param <C> A generic client-context inherited class
+ * @param <C> A generic {@link ClientContext} inherited class
  */
 public abstract class Task<C extends ClientContext> extends ClientAccessor<C> {
     public Task(C arg0) {
         super(arg0);
     }
 
+    /**
+     * Predicate to see if we can {@link #execute()} the {@link Task}
+     * @return whether the conditions for execution are met
+     */
     public abstract boolean activate();
+
+    /**
+     * executes the @link Task}. Called after {@link #activate()}
+     */
     public abstract void execute();
 }
