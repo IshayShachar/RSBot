95a504918f8556a2ce0b13532a33910a319d5a59
ishay muchtar <ishay3000@gmail.com>
2019-03-13 10:00:17 +0200

    Added some comments

--- src/Chopper/Task.java
+++ src/Chopper/Task.java
@@ -3,6 +3,10 @@ package Chopper;
 import org.powerbot.script.ClientAccessor;
 import org.powerbot.script.rt4.ClientContext;
 
+/**
+ * A class represents a task to be done on the {@link org.powerbot.script.rt4.Player}
+ * @param <C> A generic client-context inherited class
+ */
 public abstract class Task<C extends ClientContext> extends ClientAccessor<C> {
     public Task(C arg0) {
         super(arg0);
